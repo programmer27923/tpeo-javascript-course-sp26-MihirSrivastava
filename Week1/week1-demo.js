ce04eace4d97a1a2f51f5a90b69af72193355136
Ashwika Katiyar <ashwikakatiyar@gmail.com>
2026-01-29 18:43:38 -0600

    Week 1

---
+++ Week1/week1-demo.js
@@ -0,0 +1,10 @@
+// WEEK 1 DEMO: Git, running JS
+
+console.log("Hello world!");
+
+// Run the above line by clicking the triangular play button on the left side bar!
+// Check your console to see that "Hello world!" has printed.
+
+// To access the terminal, click Terminal >> New Terminal.
+// Check README for the commands to type into the terminal to push or pull from Github. 
+// Alternatively, can use VSCode's buttons via the 3 circles icon on left side bar.
